cee7c4bb158707e77ea68114a3e065f6c99c9293
Babak Aharifard <127029657+0x134134k@users.noreply.github.com>
2023-03-21 14:29:14 +0100

    Update sshd_config
    if sudo grep -Eq "^#?PasswordAuthentication\s+(no|yes)$" /etc/ssh/sshd_config; then
      # SSH config has been edited, and replace with "PasswordAuthentication yes"
      sudo sed -i -E 's/^#?PasswordAuthentication\s+(no|yes)$/PasswordAuthentication yes/' /etc/ssh/sshd_config

--- user_manager.sh
+++ user_manager.sh
@@ -9,53 +9,82 @@ echo -ne "${color}${text}\033[0m\n"
 
 echo "   User Manager Menu!"
 
-while  true;do
-# Display menu and prompt user for selection
-select option in "Change root password" "Switch to root" "Add new user" "Delete user" "Delete this script" "Exit"; do
-  case $option in
-    "Change root password")
-      # Prompt user for new root password
-      read -sp "Enter new root password: " root_password
-      echo
+while true; do
+  # Display menu and prompt user for selection
+  select option in "Change root password" "Switch to root" "Add new user" "Delete user" "Edit SSH config" "Restart SSH service" "Delete this script" "Exit"; do
+    case $option in
+      "Change root password")
+        # Prompt user for new root password
+        read -sp "Enter new root password: " root_password
+        echo
 
-      # Change root password
-      sudo passwd -u root <<< "$root_password"
-      break
-      ;;
-    "Switch to root")
-      # Switch to root user
-      su
-      break
-      ;;
-    "Add new user")
-      # Prompt user for new username and password
-      read -p "Enter username: " username
-      read -sp "Enter password: " password
-      echo
+        # Change root password
+        echo -e "$root_password\n$root_password" | sudo passwd root
+        break
+        ;;
+      "Switch to root")
+        # Prompt user for root password and switch to root if correct
+        read -sp "Enter root password: " root_password
+        echo
 
-      # Add new user
-      sudo useradd -m -s /bin/bash $username
-      echo "$username:$password" | sudo chpasswd
-      break
-      ;;
-    "Delete user")
-      # Prompt user for username to delete
-      read -p "Enter username to delete: " username
+        # Switch to root if password is correct
+        if sudo -kS whoami <<< "$root_password" && sudo -n true; then
+          sudo su -
+        else
+          echo "Incorrect password"
+        fi
+        break
+        ;;
+      "Add new user")
+        # Prompt user for new username and password
+        read -p "Enter username: " username
+        read -sp "Enter password: " password
+        echo
 
-      # Delete user
-      sudo userdel -r $username
-      break
-      ;;
-    "Delete this script")
-      # Delete this script
-      rm $0
-      echo "Script deleted."
-      break
-      ;;
-    "Exit")
-      # Exit script
-      exit;;
-    *) echo "Invalid option";;
-  esac
+        # Add new user
+        sudo useradd -m -s /bin/bash $username
+        echo "$username:$password" | sudo chpasswd
+        break
+        ;;
+      "Delete user")
+        # Prompt user for username to delete
+        read -p "Enter username to delete: " username
+
+        # Delete user
+        sudo userdel -r $username
+        break
+        ;;
+      "Edit SSH config")
+        # Check if SSH config has been edited already
+        # Check if SSH config has been edited already
+if sudo grep -Eq "^#?PasswordAuthentication\s+(no|yes)$" /etc/ssh/sshd_config; then
+  # SSH config has been edited, replace with "PasswordAuthentication yes"
+  sudo sed -i -E 's/^#?PasswordAuthentication\s+(no|yes)$/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+        else
+          # SSH config has not been edited, add a comment
+          sudo sed -i '1i# Password authentication has been disabled' /etc/ssh/sshd_config
+        fi
+        echo "SSH config updated."
+        break
+        ;;
+      "Restart SSH service")
+        # Restart SSH service
+        sudo service ssh restart
+        echo "SSH service restarted."
+        break
+        ;;
+      "Delete this script")
+        # Delete this script
+        rm $0
+        echo "Script deleted."
+        break
+        ;;
+      "Exit")
+        # Exit script
+        exit;;
+      *) echo "Invalid option";;
+    esac
+  done
 done
-done
+
